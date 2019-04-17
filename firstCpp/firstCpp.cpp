18cf07e48d470fbf7b2a6ac118c4ba3b37e7742c
Thiago Farias <thiago_fdutra@hotmail.com>
2019-04-17 00:28:01 -0300

    changes

--- firstCpp/firstCpp.cpp
+++ firstCpp/firstCpp.cpp
@@ -1,11 +1,13 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <stdio.h>
 #include <conio.h>
 
 using namespace std;
 
 bool gameOver;
-int width = 30, height = 10, x, y, fx, fy, ms = 300, score = 0;
+int width = 30, height = 10, nTail, x, y, fx, fy, ms = 200, score = 0;
+int tailX[100], tailY[100];
 
 enum eDir { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDir dir;
@@ -18,57 +20,32 @@ void start(){
 	fy = rand() % height;
 }
 
-string direction() {
-	string v = "STOP";
-	switch (dir) {
-	case LEFT:
-		v = 'l';
-		break;
-	case RIGHT:
-		v = 'r';
-		break;
-	case UP:
-		v = 'u';
-		break;
-	case DOWN:
-		v = 'd';
-		break;
-	}
-	return v;
-}
-
 void draw() {
 	system("cls");
 	
-	for (int i = 0; i <= width+2; i++) {
-		cout << '#';
-	}
+	for (int i = 0; i <= width+2; i++) cout << '#';
 	cout << endl;
-	
 	for (int j = 0; j <= height; j++) {
 		for (int i = 0; i <= width; i++) {
-			if (i == 0)
-				cout << '#';
-			
-			if (x == i && y == j)
-				cout << 'O';
-			else if (fx == i && fy == j)
-				cout << '*';
-			else
-				cout << ' ';
-
-			if (i == width)
-				cout << '#' << endl;
+			if (i == 0) cout << '#';
+			if (x == i && y == j) cout << '*';
+			else if (fx == i && fy == j) cout << 'o';
+			else {
+				bool print = false;
+				for (int k = 0; k < nTail; k++)
+				{
+					if (tailX[k] == i && tailY[k] == j) {
+						cout << "*";
+						print = true;
+					}		
+				}
+				if (!print) cout << ' ';
+			}
+			if (i == width) cout << '#' << endl;
 		}
 	}
-	for (int i = 0; i <= width+2; i++)
-		cout << '#';
-	cout << 
-		endl << 
-		"DIR: " <<
-		direction() << 
-		" | SCORE: " << 
-		score;
+	for (int i = 0; i <= width+2; i++) cout << '#';
+	cout << "SCORE: " << score;
 	Sleep(ms);
 }
 
@@ -96,20 +73,34 @@ void input() {
 }
 
 void hit() {
-	if (x == -1 || x >= width + 1) {	
-		gameOver = true;
-	}
-	if (y == -1 || y >= height + 1) {
-		gameOver = true;
-	}
+	if (x == -1 || x >= width + 1) gameOver = true;
+	if (y == -1 || y >= height + 1) gameOver = true;
 	if (fx == x && fy == y) {
-		score++;
+		nTail++;
+		score += 10;
 		fx = rand() % width;
 		fy = rand() % height;
 	}
+	for (int i = 0; i < nTail; i++)
+		if (tailX[i] == x && tailY[i] == y) gameOver = true;
 }
 
 void logic() {
+	int 
+		prevTX = tailX[0],
+		prevTY = tailY[0],
+		prevTX2, prevTY2;
+	tailX[0] = x;
+	tailY[0] = y;
+	for (int i = 1; i < nTail; i++)
+	{
+		prevTX2 = tailX[i];
+		prevTY2 = tailY[i];
+		tailX[i] = prevTX;
+		tailY[i] = prevTY;
+		prevTX = prevTX2;
+		prevTY = prevTY2;
+	}
 	switch (dir)
 	{
 	case LEFT:
